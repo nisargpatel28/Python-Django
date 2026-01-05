569ad58d5fa7c671572d1ffe5e2108f0e546dcc3
Nisarg Patel <patelnisarg.pn@gmail.com>
2026-01-05 02:24:50 -0500

    View file changes for admin purpose

--- malketai/myapp/views.py
+++ malketai/myapp/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render, redirect
+#from django.contrib.auth.models import User
 from .models import User
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import ContactForm
 # Create your views here.
 
+def admin_login(request):
+    try:
+        if request.user.is_authenticated:
+            return redirect('index')
+        #messages.info(request, 'Account not found')
+        # if request.method == 'POST':
+        #     username = request.POST.get('username')
+        #     password = request.POST.get('password')
+        #     user = authenticate(request, username=username, password=password)
+        #     if user is not None:
+        #         login(request, user)
+        #         return redirect('index')
+        #     else:
+        #         return HttpResponse("Invalid credentials")
+        # return render(request, 'users/admin_login.html')
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user_obj = User.objects.filter(username=username)
+            if not user_obj.exists():
+                messages.info(request, "User account not found")
+                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+            user_obj = authenticate(username = username, password = password)
+
+            if user_obj and user_obj.is_superuser:
+                login(request, user_obj) 
+                return redirect('index')
+        
+            messages.info(request, 'Invalid Info')
+            return redirect('/')
+
+        return render(request, 'users/admin_login.html')
+    except Exception as e:
+        messages.info(request, 'Invalid Info')
+        return redirect('/')
 
 def index(request):
     users = User.objects.all()
